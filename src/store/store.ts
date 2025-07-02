b7b187933be3b9d069dd0b0ce1b883b07c2aa1c5
RilPax <RilPax@yandex.ru>
2025-07-02 14:05:31 +0500

    store-fix

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from "./slices/auth-slice/auth-slice";
 import todoSliceReducer from './slices/todo-slice/todo-slice'
+import {
+  useDispatch as dispatchHook,
+  useSelector as selectorHook 
+} from "react-redux";
+
+import type {TypedUseSelectorHook} from 'react-redux'
 
 const rootReducer = combineReducers({
   authReducer: authSliceReducer,
@@ -12,3 +18,8 @@ export const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useDispatch: () => AppDispatch = () => dispatchHook();
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
